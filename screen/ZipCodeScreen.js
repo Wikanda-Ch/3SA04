b68ba5febfcc6771ba340267416b1a8be53e2e61
Wikanda-Ch <6110110408@psu.ac.th>
2020-08-24 01:43:42 +0700

    section 7-1

--- screen/ZipCodeScreen.js
+++ screen/ZipCodeScreen.js
@@ -1,23 +1,29 @@
 import React from 'react'
-import { FlatList, View, Text, StyleSheet } from 'react-native';
+import { FlatList, View, Text, StyleSheet, ImageBackground } from 'react-native';
 import { TouchableHighlight } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
 
+
 const availableZipItems = [
-    { place: 'Hatyai', code: '90110' },
-    { place: 'Trang', code: '92000' },
-    { place: 'Chiangmai', code: '50000' },
-    { place: 'Khonkaen', code: '40000' },
-    { place: 'Chonburi', code: '20000' },
+    { place: '𝐻𝑎𝑡𝑦𝑎𝑖', code: '90110', img:require('../hatyai.jpg') },
+    { place: '𝑇𝑟𝑎𝑛𝑔', code: '92000', img:require('../trang.jpg') },
+    { place: '𝐶ℎ𝑖𝑎𝑛𝑔𝑚𝑎𝑖', code: '50000', img:require('../chiangmai.jpg') },
+    { place: '𝐾ℎ𝑜𝑛𝑘𝑎𝑒𝑛', code: '40000', img:require('../khonkaen.jpg') },
+    { place: '𝐶ℎ𝑜𝑛𝑏𝑢𝑟𝑖', code: '20000', img:require('../chonburi.jpg') },
+    { place: '𝐵𝑎𝑛𝑔𝑘𝑜𝑘', code: '10200', img:require('../bangkok.jpg') },
+    { place: '𝑃ℎ𝑢𝑘𝑒𝑡', code: '83000', img:require('../phuket.jpg') },
+    { place: '𝑅𝑎𝑦𝑜𝑛𝑔', code: '21000', img:require('../rayong.jpg') },
 ]
 
-const ZipItem = ({place, code, navigation}) => (
+const ZipItem = ({place, code, navigation, img}) => (
     <TouchableHighlight onPress={() => {
         navigation.navigate('Weather', {zipCode: code})
     }}>
         <View style={styles.zipItem}>
-            <Text>{place}</Text>
-            <Text>{code}</Text>
+            <ImageBackground source={img} style = {styles.backdrop}>
+                <Text style = {styles.zipPlace}>{place}</Text>
+                <Text style = {styles.zipCode}>{code}</Text>
+            </ImageBackground>
         </View>
     </TouchableHighlight>
 )
@@ -40,9 +46,23 @@ const styles = StyleSheet.create({
         justifyContent: 'space-between'
     },
     zipPlace: {
+        textAlign: 'center',
+        textAlignVertical: 'center',
         flex: 1,
+        fontSize: 25,
+        fontWeight: 'bold',
+        backgroundColor: 'pink',
+        opacity: 0.6,
+        color: 'white'
     },
     zipCode: {
         flex: 1,
+        padding: 5,
+        textAlign: 'right',
+        color: 'white',
+    },
+    backdrop: {
+        width: '100%',
+        height: 200
     }
 })
